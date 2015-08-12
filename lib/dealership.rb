712498b415c0f882580adcd7a726fa35d09ffa86
Josue Valverde & Justin Scott <josueandjustin@fake.com>
2015-08-12 10:10:43 -0700

    add and pass spec that returns id of dealership.

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -3,9 +3,15 @@ class Dealership
 
   define_method(:initialize) do |name|
     @name = name
+    @id = @@dealerships.length() + 1
   end
 
   define_method(:name) do
     @name
   end
+
+  define_method(:id) do
+    @id
+  end
+
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -9,4 +9,11 @@ describe(Dealership) do
     end
   end
 
+  describe('#id') do
+    it('will return the id of the dealership') do
+      test_dealership = Dealership.new("Bob's Car Lot")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+
 end
