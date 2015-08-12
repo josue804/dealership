22605d6d4e0915aa0cb498b498a916a40ef4ad1e
Josue Valverde & Justin Scott <josueandjustin@fake.com>
2015-08-12 14:45:35 -0700

    have html pages for sinatra integration

--- app.rb
+++ app.rb
@@ -23,3 +23,31 @@ post('/dealerships') do
   @dealerships = Dealership.all()
   erb(:success)
 end
+
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:vehicles)
+end
+
+get('/dealerships/:id/vehicles/new') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:vehicles_form)
+end
+
+post('/dealerships/:id/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  color = params.fetch('color')
+  @vehicle = Vehicle.new(make, model, year, color)
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:vehicles)
+end
+
+get('/vehicles/:id/:dealership_id') do
+  @vehicle = Vehicle.find(params.fetch('id').to_i())
+  @dealership = params.fetch('dealership_id').to_i()
+  erb(:vehicle_info)
+end
