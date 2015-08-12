8b26ba18cd18590ac6df463800046a978668245e
Josue Valverde & Justin Scott <josueandjustin@fake.com>
2015-08-12 09:22:56 -0700

    Pass second spec to return the model

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,9 +3,16 @@ require('dealership')
 
 describe(Vehicle) do
   describe('#make') do
-    it('will create a new vehicle') do
+    it('will return the make of a vehicle') do
       test_vehicle = Vehicle.new('Toyota', 'Prius', 2015, 'white')
       expect(test_vehicle.make()).to(eq('Toyota'))
+    end
+  end
+
+  describe('#model') do
+    it('will return the model of a vehicle') do
+      test_vehicle = Vehicle.new('Toyota', 'Prius', 2015, 'white')
+      expect(test_vehicle.model()).to(eq('Prius'))
     end
   end
 end
